566b97c1683566ad14b544ffaf9ec4512ccbff0e
Vova Melnyk <hagmen4339@gmail.com>
2023-11-21 17:20:10 +0200

    add project files

---
+++ 19-Project (footer)/finish code/index.js
@@ -0,0 +1,10 @@
+// const hw = 
+
+// const result = hw.filter(
+//   (el) => el.statusChangeDateTime >= "2023-10-01T00:00:00" || el.statusChangeDateTime <= "2023-10-31T23:59:59"
+// );
+
+// console.log(result);
+// const test = hw.map((el) => el.studentUsername);
+// console.log(test);
+// console.log(new Set(test));
